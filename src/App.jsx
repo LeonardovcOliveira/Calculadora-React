839c9fbffe1298f821bac14332558bdbfffc9b36
Leonardo Oliveira <leonardo.vinicius98@hotmail.com>
2024-11-08 15:43:41 -0300

    primeira versao

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+
+
+export default function App(){
+  const [primeiroValor, setPrimeiroValor] = useState();
+  const [segundoValor, setSegundoValor] = useState();
+  const [resultado, setResultado] = useState();
+
+  const capturandoPrimeiroValor = (e) => {
+    setPrimeiroValor(Number(e.target.value));
+  };
+
+  const capturandoSegundoValor = (e) => {
+    setSegundoValor(Number(e.target.value))
+  };
+
+  const soma = () => {
+    setResultado(primeiroValor + segundoValor);
+  };
+
+  const subtracao = () => {
+    setResultado(primeiroValor - segundoValor);
+  };
+
+  const multiplicacao = () => {
+    setResultado(primeiroValor * segundoValor);
+  };
+
+  const divisao = () => {
+    setResultado(primeiroValor / segundoValor);
+  };
+  return(
+    <main>
+      
+      <h1>Calculadora</h1>
+      <input type="number" onChange={capturandoPrimeiroValor}/>
+      <input type="number" onChange={capturandoSegundoValor}/>
+      <section>
+      <button onClick={soma}>+</button>
+      <button onClick={subtracao}>-</button>
+      <button onClick={multiplicacao}>x</button>
+      <button onClick={divisao}>/</button>
+      </section>
+      <h2>Resultado: {resultado}</h2>
+    </main>
+  );
+}
